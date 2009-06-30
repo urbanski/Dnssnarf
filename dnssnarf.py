7768fc42efa9dac27a364d6059ed4949294e9753
grantstavely <grant@grantstavely.com>
2009-07-01 05:42:35 +0800

    This still barfs the output a bit when it gets an SOA RR. Will fix.
    scapy can't do much with TCP DNSRRs from what I can tell. Oh well.

--- dnssnarf.py
+++ dnssnarf.py
@@ -1,9 +1,24 @@
+#!/usr/bin/python
 from scapy import *
+import time
 
 def dns_callback(pkt):
-   if DNS in pkt and pkt[UDP].dport == 53:
-		print pkt.sprintf("client %IP.src%#%UDP.sport%: query: ") + \
-			pkt[DNSQR].qname + " " +  pkt[DNSQR].sprintf("%qclass% %qtype% +")
+  if DNSQR in pkt and pkt.dport == 53:
+    print str(time.asctime( time.localtime(time.time()) )) + \
+      " session-id: " + str(pkt[DNS].id) + \
+      " UDP client: " + pkt[IP].src + ":" + str(pkt.sport) + \
+      " server: " + pkt[IP].dst + ":" + str(pkt.dport) + \
+      " query: " + pkt[DNSQR].qname + \
+      " class: " + pkt[DNSQR].sprintf("%qclass%") + \
+      " type: " + pkt[DNSQR].sprintf("%qtype%")
+  elif DNSRR in pkt and pkt.sport == 53:
+    print str(time.asctime( time.localtime(time.time()) )) + \
+      " session-id: " + str(pkt[DNS].id) + \
+      " UDP server: " + str(pkt[IP].src) + ":" + str(pkt.sport) + \
+      " response: " + pkt[DNSRR].rdata + \
+      " class: " + pkt[DNSRR].sprintf("%rclass%") + \
+      " type: " + pkt[DNSRR].sprintf("%type%") + \
+      " ttl: " + str(pkt[DNSRR].ttl) + \
+      " len: " + str(pkt[DNSRR].rdlen) 
 
-iface = (sys.argv[1] if (len(sys.argv) > 1) else 'eth0')
-sniff(iface=iface, prn=dns_callback, filter="udp and port 53", store=0)
+sniff(prn=dns_callback, filter="udp and port 53", store=0)
